356b073c55a0be1033aa0a9b6fccec2a018459bf
UjwalManjunath <muddu1990@gmail.com>
2013-02-10 02:13:02 -0500

    added header and footer

--- config/config.php
+++ config/config.php
@@ -1,12 +1,11 @@
 <?php
 
 define('SQL_HOST','localhost');
-define('SQL_USER','a9102828_ujwal');
-define('SQL_PASS','muddu_1990');
-define('SQL_DB','a9102828_forum');
+define('SQL_USER','Yourmysqlusername');
+define('SQL_PASS','Yourmysqlpassword');
+define('SQL_DB','NameofDatabse');
 
-define('RE_PUBLIC_KEY','6Le_0dYSAAAAAELkEUYyqKHgEechNzjdD_mS53Ri');
-define('RE_PRIVATE_KEY','6Le_0dYSAAAAALr91d8LtqutMxiYNh1aGtMJVgsl');
+
 
 $conn=new mysqli(SQL_HOST, SQL_USER, SQL_PASS, SQL_DB) or
   die ("Hey loser, check your server connection.");		
