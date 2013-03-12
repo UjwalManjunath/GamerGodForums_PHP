b7359c8fbc3744b71f3ac43c8138aed4b331f2d8
UjwalManjunath <muddu1990@gmail.com>
2013-03-12 01:39:26 -0400

    updated redirect

--- logout.php
+++ logout.php
@@ -3,5 +3,5 @@ session_start();
 session_destroy();
 setcookie('keep_meuser', '', time() - 1*24*60*60);
 setcookie('keep_mepass', '', time() - 1*24*60*60);
-header( 'Location: https://mweigle418.cs.odu.edu/~umanjuna/proj4/index.php' )
+header( 'Location: index.php' )
 ?>
